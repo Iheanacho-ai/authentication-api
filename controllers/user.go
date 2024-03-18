b69cb85e0b540d8c19927c64a46d1ee9a185a733
Amarachi Iheanacho <amarachi2812@gmail.com>
2024-03-18 22:09:18 +0100

    added logout and login

--- controllers/user.go
+++ controllers/user.go
@@ -2,25 +2,28 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
-	"github.com/Iheanacho-ai/authentication-api.git/models"
 	"github.com/Iheanacho-ai/authentication-api.git/initializers"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/Iheanacho-ai/authentication-api.git/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func SignUp(c *gin.Context){
+func SignUp(c *gin.Context) {
 	//collect data from body parameters
 
 	var body struct {
-		Email string
+		Email    string
 		Password string
 	}
 
 	if c.Bind(&body) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read the body",
-		}) 
+		})
 
 		return
 
@@ -32,15 +35,15 @@ func SignUp(c *gin.Context){
 
 	if user.ID != 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error" : "Email Address is alread in use",
+			"error": "Email Address is alread in use",
 		})
 
 		return
 	}
-	
+
 	// hash password
 	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
-    if err != nil {
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to hash password",
 		})
@@ -50,15 +53,15 @@ func SignUp(c *gin.Context){
 	user = models.User{Email: body.Email, Password: string(hash)}
 
 	result := initializers.DB.Create(&user)
-	
+
 	if result.Error != nil {
 		c.JSON(
-			http.StatusBadRequest, 
+			http.StatusBadRequest,
 			gin.H{
 				"error": "Failed to create user",
 			})
 	}
-	
+
 	//respond
 
 	c.JSON(http.StatusOK, gin.H{
@@ -66,6 +69,71 @@ func SignUp(c *gin.Context){
 	})
 }
 
-func Login(){
+func Login(c *gin.Context) {
+	// collect your data
+	var body struct {
+		Email    string
+		Password string
+	}
 
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read the body",
+		})
+
+		return
+
+	}
+
+	// Search if the user
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Email Address does not exist on the database",
+		})
+
+		return
+	}
+
+	// Compare sent in pass with saved user pass hash
+	err := bcrypt.CompareHashAndPassword([]byte(body.Password), []byte(user.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Password is incorrect",
+		})
+
+		return
+	}
+
+	// Generate a jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Error signing in",
+		})
+	}
+
+	// Create a cookie / Send it back
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+	c.JSON(http.StatusOK, gin.H{})
 }
+
+func Logout(c *gin.Context) {
+	c.SetCookie("Authorization", "", 1 , "", "", false, true)
+}
+
+
+//ResetPassword
+//ForgotPassword
+// Validate
